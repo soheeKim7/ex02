888880d6d8c2b31baa215a5a5bc44719cee4381c
kim sohee <okoksohee@naver.com>
2023-02-22 17:42:57 +0900

    crud/create(insert),read,update,delete ->DML (Manipulation) 조작어 완성
    DDL(Definition) 정의어
    create,drop, rename,alter,truncate
    테이블을 생성,삭제,변경,이름변경 등등

--- ex02/src/main/java/org/zerock/mapper/BoardMapper.java
+++ ex02/src/main/java/org/zerock/mapper/BoardMapper.java
@@ -7,7 +7,32 @@ import org.zerock.domain.BoardVO;
 
 public interface BoardMapper {
 	
-	@Select("select * from tbl_board")
+	//게시물 목록 가져오기
+	//@Select("select * from tbl_board")
 	List<BoardVO> getList();  //게시물 목록 가져오기
 	//인터페이스는 생략해도 기본적으로 메소드 public!!
+	
+	//게시글 등록 return int 등록된 글의 개수
+//	void insert(BoardVO vo);
+	int insert(BoardVO vo); //삽입된 행의 개수!
+	
+	//게시글 수정
+	int update(BoardVO vo);
+	
+	//게시글 삭제
+	int delete(Long bno);
+	
+	//게시글 읽기(조회)
+	BoardVO read(Long bno);
+	
+	//게시글 등록시 등록된 번호 알아서 글 등록하기  //return int 등록된 글의 개수
+	int insertSelectKey(BoardVO vo);
+	
+	//bonus 등록할 글번호 확인하기  //return long 등록할 글번호
+	long mySelectKey(); 
+	
+	//bonus 확인된 글번호로 글 등록하기  //return int 등록된 글의 개수
+	int myInsert(BoardVO vo);
+	
+	
 }
--- ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
+++ ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -24,7 +24,59 @@ public class BoardMapperTests {
 //		for(BoardVO vo:mapper.getList()) 
 //			log.info(vo);		
 //		위와 동일한 의미
-		mapper.getList().forEach(vo -> log.info(vo));
-		
+		mapper.getList().forEach(vo -> log.info(vo));		
 	}
+	
+	@Test
+	public void testInsert() {
+		BoardVO vo = new BoardVO();
+		vo.setTitle("새로 작성하는 글");
+		vo.setContent("새로 작성하는 내용");
+		vo.setWriter("newbie");
+//		mapper.insert(vo);
+		log.info("삽입된 행의 개수 : "+mapper.insert(vo));
+	}
+	
+	@Test
+	public void testInsertSelectKey() {
+		BoardVO vo = new BoardVO();
+		vo.setTitle("새로 작성하는 글");
+		vo.setContent("새로 작성하는 내용");
+		vo.setWriter("newbie");
+		log.info("삽입된 행의 개수 : "+mapper.insertSelectKey(vo));
+		log.info(vo);
+	}
+	
+	@Test
+	public void testmyInsert() {
+		BoardVO vo = new BoardVO();
+		vo.setTitle("새로 작성하는 글");
+		vo.setContent("새로 작성하는 내용");
+		vo.setWriter("newbie");
+		vo.setBno(mapper.mySelectKey());
+		log.info("삽입된 행의 개수 : "+mapper.myInsert(vo));
+		log.info(vo);
+	}
+	
+	@Test
+	public void testUpdate() {
+		BoardVO vo = new BoardVO();
+		vo.setTitle("수정 작성하는 글");
+		vo.setContent("수정 작성하는 내용");
+		vo.setWriter("수정~");
+		vo.setBno(2L); //뒤에 붙은 L은 Long을 의미!
+		log.info("수정된 행의 개수 : "+mapper.update(vo));
+		log.info(vo);
+	}
+	
+	@Test
+	public void testDelete() {
+		log.info("삭제된 행의 개수 : "+mapper.delete(3L));
+	}
+	
+	@Test
+	public void testRead() {
+		log.info("읽어온 행 : "+mapper.read(4L));
+	}
+	
 }
